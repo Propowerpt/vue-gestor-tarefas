6dbb841bd377e527c6e87a3711ae0f15ea6b3765
Diogo <lisboadiogo2010@hotmail.com>
2023-11-09 15:16:19 +0000

    bloqueio da rota das tarefas quando não está autenticado

--- src/main.js
+++ src/main.js
@@ -7,10 +7,29 @@ import Home from './components/Home.vue'
 import ListaTarefas from './components/ListaTarefas.vue'
 import LoginForm from './components/LoginForm.vue'
 
+function isAutheticated(){
+  return (localStorage.getItem("token") == null || localStorage.getItem("token") == '') ? false : true;
+}
+
 const routes = [
-  { path: '/', component: Home },
-  { path: '/login', component: LoginForm },
-  { path: '/list', component: ListaTarefas },
+  { 
+    path: '/', 
+    component: Home },
+  { 
+    path: '/login', 
+    component: LoginForm 
+  },
+  { 
+    path: '/list', 
+    component: ListaTarefas,
+    //Verificar se está autenticado
+    beforeEnter: (to, from) => {
+      if(!isAutheticated()){
+      alert("É preciso fazer login antes de prosseguir !!!");
+      return '/login'
+      }
+    },
+  },
 ]
 
 const router = createRouter({
